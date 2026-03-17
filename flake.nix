0331c77aee94d864f17582565b8ac2901938959a
joaosimsic <jpsimsic@hotmail.com>
2026-03-17 11:29:50 -0300

    feat: dotfiles as submodule

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "NixOS config";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./hosts/nixos/configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.extraSpecialArgs = { inherit inputs; };
+          home-manager.users.joao = {
+            imports = [ 
+              ./home.nix 
+            ];
+          };
+        }
+      ];
+    };
+  };
+}
---
+++ modules/nvim.nix
@@ -0,0 +1,14 @@
+{ pkgs, config, ... }:
+
+{
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    viAlias = true;
+    vimAlias = true;
+  };
+
+  xdg.configFile."nvim" = {
+    source = config.lib.file.mkOutOfStoreSymlink "/home/joao/proj/nixos/dotfiles/.config/nvim";
+  };
+}
