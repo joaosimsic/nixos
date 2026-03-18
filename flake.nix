c3f10a0c001e34ef4c5ff1d044c3a04f8b697be7
joaosimsic <jpsimsic@hotmail.com>
2026-03-17 21:19:04 -0300

    feat: system in flake

--- flake.nix
+++ flake.nix
@@ -12,6 +12,7 @@
 
   outputs = { self, nixpkgs, home-manager, ... }@inputs: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
       specialArgs = { inherit inputs; };
       modules = [
         ./hosts/nixos/configuration.nix
