b1a4c8c86a22a2e8ad04aa1f29e1631a7b8dc61c
joaosimsic <jpsimsic@hotmail.com>
2026-03-19 21:37:11 -0300

    feat: working personal config

--- hosts/personal/configuration.nix
+++ hosts/personal/configuration.nix
@@ -10,16 +10,22 @@
   networking.hostName = hostname;
 
   boot.kernelPackages = pkgs.linuxPackages_latest;
-  
+  boot.kernelParams = [ "amd_pstate=active" ];
+
   boot.initrd.kernelModules = [ "amdgpu" ];
   services.xserver.videoDrivers = [ "amdgpu" ];
 
   hardware.graphics = {
     enable = true;
-    enable32Bit = true; 
+    enable32Bit = true;
+    extraPackages = with pkgs; [ rocmPackages.clr.icd ];
   };
 
   hardware.cpu.amd.updateMicrocode = true;
+
+  zramSwap.enable = true;
+
+  services.fstrim.enable = true;
 
   system.stateVersion = "24.11";
 }
--- hosts/personal/hardware-configuration.nix
+++ hosts/personal/hardware-configuration.nix
@@ -2,11 +2,14 @@
 {
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
-    (modulesPath + "/profiles/all-hardware.nix")
   ];
 
-  boot.initrd.availableKernelModules = [ ];
-  boot.kernelModules = [ ];
+  boot.initrd.availableKernelModules = [
+    "xhci_pci" "ahci" "nvme" "usb_storage" "usbhid" "sd_mod"
+  ];
+  boot.kernelModules = [ "kvm-amd" ];
+
+  hardware.enableRedistributableFirmware = true;
 
   fileSystems."/" = {
     device = "/dev/disk/by-label/nixos";
